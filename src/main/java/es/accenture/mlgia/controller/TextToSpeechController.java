acfb87440a9a873d4081d29dc60f802eb4555f23
SirNeo <lepeonline@gmail.com>
2019-05-21 23:43:35 +0200

    generate audio in mp3 format

--- src/main/java/es/accenture/mlgia/controller/TextToSpeechController.java
+++ src/main/java/es/accenture/mlgia/controller/TextToSpeechController.java
@@ -1,15 +1,17 @@
 package es.accenture.mlgia.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import es.accenture.mlgia.audio.Audio;
 import es.accenture.mlgia.dto.InputTextToSpeechDTO;
 import es.accenture.mlgia.dto.TextToSpeechDTO;
 import es.accenture.mlgia.watson.TextToSpeechMlgia;
@@ -28,31 +30,50 @@ public class TextToSpeechController    {
     	log.debug("Inicio info()");
     	return sMensajeInfo;
     }
+    
+    @RequestMapping(value = "/voices", method = RequestMethod.GET)
+    public String getVoices() {
+    	log.debug("get Voices from API");
+    	String voices;
+		try {
+			voices = textToSpeech.getVoices();
+			return voices;
+		} catch (IOException e) {
+			log.error("Error recuperando las voces");
+		}
+		return null;
+    }
+    
+    @RequestMapping(value = "/audio2", method = RequestMethod.GET)
+    public String getAudio() {
+    	try {
+			textToSpeech.getAudioFile("Hola, esto es una prueba");
+		} catch (IOException e) {
+			log.error("Error obteniendo el audio");
+		}
+    	return "Peticion hecha. Comprueba el audio guardado";
+    }
+    
+    @RequestMapping(value = "/audio3/{text}", method = RequestMethod.GET)
+    public String getAudio(@PathVariable("text") String text) {
+    	try {
+			textToSpeech.getAudioFileAndSaveInLocal(text);
+		} catch (IOException e) {
+			log.error("Error obteniendo el audio");
+		}
+    	return "Peticion hecha. Comprueba el audio guardado";
+    }
 
     @Autowired
     TextToSpeechMlgia textToSpeech;
-
-    @RequestMapping(value = "/audiojava", method = RequestMethod.POST)
-	public void getAudioJava(HttpServletResponse response,
-			@RequestBody InputTextToSpeechDTO text) throws Exception
-	{
-		response.setHeader("Content-Disposition","attachment;filename=audio.wav");
-		response.setContentType("audio/wave");
-
-		Audio.getStream(response.getOutputStream(),text.getText());
-	}
-
 
     @RequestMapping(value = "/audio", method = RequestMethod.POST)
    	public TextToSpeechDTO  getAudioWatson(HttpServletResponse response,
    			@RequestBody InputTextToSpeechDTO text) throws Exception
 	{
     	log.debug("Llamada a Audio con texto:" + text);
-    	if (text != null)
-    	{
-    		response.setHeader("Content-Disposition","attachment;filename=audio.wav");
-    		response.setContentType("audio/wave");
-
+    	if (text != null) {
+ 
     		byte[] fileOutput = textToSpeech.getAudioFile(text.getText());
 
     		TextToSpeechDTO textToSpeech = TextToSpeechDTO.builder()
--- src/main/java/es/accenture/mlgia/watson/TextToSpeechMlgia.java
+++ src/main/java/es/accenture/mlgia/watson/TextToSpeechMlgia.java
@@ -1,17 +1,21 @@
 package es.accenture.mlgia.watson;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Base64;
+import java.io.OutputStream;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
-import com.ibm.watson.developer_cloud.text_to_speech.v1.model.AudioFormat;
-import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
-import com.ibm.watson.developer_cloud.text_to_speech.v1.util.WaveUtils;
+import com.ibm.cloud.sdk.core.service.security.IamOptions;
+import com.ibm.watson.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.text_to_speech.v1.model.SynthesizeOptions;
+import com.ibm.watson.text_to_speech.v1.model.SynthesizeOptions.Voice;
+import com.ibm.watson.text_to_speech.v1.model.Voices;
+import com.ibm.watson.text_to_speech.v1.util.WaveUtils;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -19,43 +23,102 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class TextToSpeechMlgia {
-
-	@Value("${watson.text.to.speech.username}")
+	
+	@Value("${watson.apikey}")
 	@Getter
-	private String usuario;
-
-	@Value("${watson.text.to.speech.password}")
+	private String apikey;
+	
+	@Value("${watson.endpoint}")
 	@Getter
-	private String password;
-
-	@Value("${language.model}")
-	private String languageModel;
-
-	TextToSpeech speechService;
-
+	private String endpoint;
+	
+	@Value("${watson.voice}")
+	private String voice;
+	
 	public byte[] getAudioFile(String text) throws IOException {
-		log.debug("Se obtiene audio para el texto:" + text);
-		speechService = new TextToSpeech(usuario, password);
-		InputStream in = null;
-		byte[] salida = null;
-		byte[] encoded = null;
-		InputStream stream = null;
-		try {
-
-			stream = speechService.synthesize(text, Voice.ES_ENRIQUE, AudioFormat.WAV).execute();
-			in = WaveUtils.reWriteWaveHeader(stream);
-			salida = IOUtils.toByteArray(in);
-			encoded = Base64.getEncoder().encode(salida);
-		} catch (Exception e) {
-			log.debug("Se ha producio un error al obtener el audio." + e.getMessage());
-			e.printStackTrace();
-		} finally {
-			in.close();
-			stream.close();
+		log.debug("getAudioFile() init");
+		
+		IamOptions options = new IamOptions.Builder()
+			    .apiKey(apikey)
+			    .build();
+		
+		TextToSpeech service = new TextToSpeech(options);
+		service.setEndPoint(endpoint);
+		
+		if (text.contains(":")) {
+			text = text.substring(0, text.indexOf(":"));
 		}
-
-		log.debug("Se retorna audio");
-		return encoded;
+		
+		SynthesizeOptions synthesizeOptions = new SynthesizeOptions.Builder()
+				  .text(text)
+				  .voice(Voice.ES_ES_LAURAVOICE)
+				  .accept(SynthesizeOptions.Accept.AUDIO_MP3)
+				  .build();
+		
+		InputStream stream = service.synthesize(synthesizeOptions).execute().getResult();
+		InputStream in = WaveUtils.reWriteWaveHeader(stream);
+		stream.close();
+		  
+		return IOUtils.toByteArray(in);
+	}
+	
+	private void saveAudio(InputStream in, String filename) {
+		OutputStream out;
+		try {
+			out = new FileOutputStream(filename);
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = in.read(buffer)) > 0) {
+				out.write(buffer, 0, length);
+			}
+			out.close();
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getVoices() throws IOException {
+		
+		IamOptions options = new IamOptions.Builder()
+			    .apiKey(apikey)
+			    .build();
+		TextToSpeech service = new TextToSpeech(options);
+		service.setEndPoint(endpoint);
+		
+		Voices voices = service.listVoices().execute().getResult();
+		//System.out.println(voices);
+		return voices.toString();
+	}
+	
+	public void getAudioFileAndSaveInLocal(String text) throws IOException {
+		log.debug("getAudioFileAndSaveInLocal() init");
+		
+		IamOptions options = new IamOptions.Builder()
+			    .apiKey(apikey)
+			    .build();
+		
+		TextToSpeech service = new TextToSpeech(options);
+		service.setEndPoint(endpoint);
+		
+		if (text.contains(":")) {
+			text = text.substring(0, text.indexOf(":"));
+		}
+		
+		SynthesizeOptions synthesizeOptions = new SynthesizeOptions.Builder()
+				  .text(text)
+				  .voice(Voice.ES_ES_LAURAVOICE)
+				  .accept(SynthesizeOptions.Accept.AUDIO_MP3)
+				  .build();
+		
+		InputStream stream = service.synthesize(synthesizeOptions).execute().getResult();
+		InputStream in = WaveUtils.reWriteWaveHeader(stream);
+		stream.close();
+		
+		saveAudio(in, "test_text_to_speech.mp3");
+		in.close();
 	}
 
 }
